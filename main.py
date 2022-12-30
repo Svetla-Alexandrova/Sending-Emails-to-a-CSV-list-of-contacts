7114a0f75b1b32ca1ed8bc6790fa5242ed650b09
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-30 10:37:04 +0000

    Sending emails to list of contacts with dynamic message

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import yagmail
 import os
-import time
+import pandas
+
 
 sender = 'mmmm@gmail.com'
 receiver = 'nnn@gmail.com'
 
 subject = 'This is the subject'
-content = """
-Here is the content of the email
-"""
-
 
 yag = yagmail.SMTP(user=sender, password=os.getenv("PASSWORD"))
-# First select Tools/Secrets; Give name to the key, then go to your gmail and apply apps password
-# Uses getenv to prevent the vialization of password from other users.
+# # First select Tools/Secrets; Give name to the key, then go to your gmail and apply apps password
+# # Uses getenv to prevent the vialization of password from other users.
 
-while True:
-  yag.send(to=receiver, subject=subject, contents=content)
+df = pandas.read_csv('contacts.csv')
+# print(df)
+
+for index, row in df.iterrows():
+  content = f"Hello, {row['name']}, Here is the content of the email"
+
+  yag.send(to=row['email'], subject=subject, contents=content)
   print('Email is sent')
-  time.sleep(60)
